ad6a9f73478b389bbf9c7f6635813e0b809f12d0
o-dim <omiia0407@gmail.com>
2023-05-15 14:42:18 +0900

    xml edit

--- veggie/src/main/java/com/gdu/semi/controller/QnaController.java
+++ veggie/src/main/java/com/gdu/semi/controller/QnaController.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gdu.semi.service.QnaService;
 
+@RequestMapping("/qna")
 @Controller
 public class QnaController {
 	@Autowired
 	private QnaService qnaService;
 	
-	@GetMapping("/qna/list.do")
+	@GetMapping("/list.do")
 	public String qnaList(Model model) {
 		model.addAttribute("qnaList", qnaService.getQnaList());
 		return "qna/list";
--- veggie/src/main/java/com/gdu/semi/intercept/nocontent.java
+++
@@ -1,5 +0,0 @@
-package com.gdu.semi.intercept;
-
-public class nocontent {
-	// 안 쓰는 파일입니다 삭제할거에요!
-}
